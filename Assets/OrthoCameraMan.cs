bbcf25bcd270bb831d486a880ab2dfb7e0d53683
Finlay Norton-Lindsay <p3n.duke@gmail.com>
2021-03-02 14:06:51 -0800

    Expanded Compund
    Added Rooms,
    Lighting,
    Extra Assets,
    Furniature

---
+++ Assets/OrthoCameraMan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrthoCameraMan : MonoBehaviour
+{
+    public int distance;
+    // x^2 + 9x^2 = y^2
+    
+    void Update()
+    {
+        int proportion = 3;
+        float floatX = Mathf.Sqrt((distance^2)/(proportion^2+1));
+        this.gameObject.transform.position = new Vector3(floatX, 3 * floatX, floatX);
+        Debug.Log(floatX);
+    }
+}
